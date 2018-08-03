86ae0f64fbaca64bbf251d2987979efeac8c79fd
McL <mcl@brm>
2018-08-03 11:44:08 +0300

    polymorphism example

---
+++ polymorphism.swift
@@ -0,0 +1,29 @@
+class Shape
+{
+    var area: Double?
+    
+    func calculateArea(valA: Double, valB: Double)
+    {
+        
+    }
+}
+
+class Triangle: Shape
+{
+    override func calculateArea(
+        valA: Double,
+        valB: Double)
+    {
+        area = (valA * valB) / 2
+    }
+}
+
+class Rectangle: Shape
+{
+    override func calculateArea(
+        valA: Double,
+        valB: Double)
+    {
+        area = valA * valB
+    }
+}
